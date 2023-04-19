bed3a479015cdbf7492dc3377fc406a24a8fac4e
Tasnim Niloy <promass.nt@gmail.com>
2023-04-18 23:19:25 -0400

    Changes

--- app/views/Hotspot/Index.php
+++ app/views/Hotspot/Index.php
@@ -9,6 +9,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../../../css/navbar.css">
+    <link rel="stylesheet" href="../../../css/saved.css">
 </head>
 
 <body style="background-color: #121212;">
@@ -54,9 +55,69 @@
 
     <!-- CONTENT -->
 
-    Hotspot Under Constructions!
+    <div class="container Saved-mother-box">
+        <div class="row">
+            <div class="col-lg-6 Saved-map-box-m">
+                <div class="Saved-map-box-c">
+                    <div id="Map-google" class="Saved-map"></div>
+                </div>
+            </div>
+            <div class="col-lg-6 Saved-list-box-m">
+                <div class="Saved-list-box-c">
+                    <div>
+                        <h4>Saved Locations</h3>
+                    </div>
+                    <div class="Saved-item-box" style="padding-right: 10px;">
+                            <div class="Saved-item-c" style="margin-bottom: 10px;" onclick="showSaved(45.50070679, -73.56870186)">
+                                <div>Near: Place Ville Marie</div>
+                                <div>
+                                    <span>Longitude: -73.56870186</span>
+                                    |
+                                    <span>Latitude: 45.50070679</span>
+                                </div>
+                                <div>Parking Rules: P EN TOUT TEMPS</div>
+                                <div>Area: Ville-Marie</div>
+                            </div>
+                            <div class="Saved-item-c" style="margin-bottom: 10px;" onclick="showSaved(45.50459227, -73.55553697)">
+                                <div>Near: Notre-Dame Basilica</div>
+                                <div>
+                                    <span>Longitude: -73.55553697</span>
+                                    |
+                                    <span>Latitude: 45.50459227</span>
+                                </div>
+                                <div>Parking Rules: P 09h-19h SAM.</div>
+                                <div>Area: Ville-Marie</div>
+                            </div>
+                            <div class="Saved-item-c" style="margin-bottom: 10px;" onclick="showSaved(45.49518038, -73.65672237)">
+                                <div>Near: Gibeau Orange Julep</div>
+                                <div>
+                                    <span>Longitude: -73.65672237</span>
+                                    |
+                                    <span>Latitude: 45.49518038</span>
+                                </div>
+                                <div>Parking Rules: A EN TOUT TEMPS</div>
+                                <div>Côte-des-Neiges - Notre-Dame-de-Grâce</div>
+                            </div>
+                            <div class="Saved-item-c" style="margin-bottom: 10px;" onclick="showSaved(45.55960228, -73.5480317)">
+                                <div>Near: Biodome</div>
+                                <div>
+                                    <span>Longitude: -73.5480317</span>
+                                    |
+                                    <span>Latitude: 45.55960228</span>
+                                </div>
+                                <div>Parking Rules: P EN TOUT TEMPS</div>
+                                <div>Mercier - Hochelaga-Maisonneuve</div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     
     <!-- CONTENT -->
+
+    <script async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD3FRojcfkJxd8nVBad0DLAeneZcKYbuJE&libraries=places&callback=initGoogle"></script>
+    <script type="text/javascript" src="../../../js/SavedMap.js"></script>
 
 </body>
 
