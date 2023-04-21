b1c9e64268801fdc4142576753fb6eaca9856b9c
Tasnim Niloy <promass.nt@gmail.com>
2023-04-20 23:31:01 -0400

    Changes

--- app/controllers/SavedController.php
+++ app/controllers/SavedController.php
@@ -4,18 +4,18 @@ class SavedController extends Controller {
 
     public function index() {
         if (isset($_SESSION['user_id'])) {
-            $home = $this->model('home');
+            $home = $this->model('Home');
             $data = $home->getSchedulesForUser($_SESSION['user_id']);
 
             $this->view('Saved/Index', $data);
         }
         else {
-            headear('location: /Authentication/Login');
+            header('location: /Authentication/Login');
         }
     }
 
     public function save($sid) {
-        $saved = $this->model('saved');
+        $saved = $this->model('Saved');
         $saved->uid = $_SESSION['user_id'];
         $saved->sid = $sid;
         $saved->saveSchedule();
@@ -24,7 +24,7 @@ class SavedController extends Controller {
     }
 
     public function delete($sid) {
-        $saved = $this->model('saved');
+        $saved = $this->model('Saved');
         $saved->uid= $_SESSION['user_id'];
         $saved->sid = $sid;
         $saved->removeSavedSchedule();
